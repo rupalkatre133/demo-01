c75e0cee9d9f5d2041454e70fb5162fd5f45456d
rupalkatre133 <rupalkatre133@gmail.com>
2025-11-19 16:30:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,79 @@ resource "aws_volume_attachment" "example_attach" {
     aws_ebs_volume.example_volume
   ]
 }
+
+# --------------------------
+#   SECURITY GROUP FOR ALB
+# --------------------------
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALB-SG"
+  }
+}
+
+# --------------------------
+#   APPLICATION LOAD BALANCER
+# --------------------------
+resource "aws_lb" "application_lb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.lb_subnets
+
+  tags = {
+    Name = "My-ALB"
+  }
+}
+
+# --------------------------
+#   TARGET GROUP
+# --------------------------
+resource "aws_lb_target_group" "alb_tg" {
+  name        = "my-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+# --------------------------
+#   REGISTER EC2 WITH ALB
+# --------------------------
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_instance.example.id
+  port             = 80
+}
+
+# --------------------------
+#   LISTENER (ALLOW HTTP)
+# --------------------------
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.application_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
